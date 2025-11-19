4e9f4ea3f83dfe2712c8971484cfa66a030fb6dc
kajaclarium <kajamoideena@clarium.tech>
2025-11-19 13:29:18 +0530

    sw changes

--- src/Serviceworkerregistration.js
+++ src/Serviceworkerregistration.js
@@ -1,41 +1,5 @@
-// serviceWorkerRegistration.js
+import { registerSW } from 'virtual:pwa-register';
 
-export function register() {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/service-worker.js")
-          .then((registration) => {
-            console.log("SW registered:", registration);
-  
-            // Listen for updates
-            registration.onupdatefound = () => {
-              const installingWorker = registration.installing;
-              if (installingWorker) {
-                installingWorker.onstatechange = () => {
-                  if (installingWorker.state === "installed") {
-                    if (navigator.serviceWorker.controller) {
-                      console.log("New content available; please refresh.");
-                    } else {
-                      console.log("Content cached for offline use.");
-                    }
-                  }
-                };
-              }
-            };
-          })
-          .catch((error) => {
-            console.error("SW registration failed:", error);
-          });
-      });
-    }
-  }
-  
-  export function unregister() {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.ready.then(reg => {
-        reg.unregister();
-      });
-    }
-  }
-  
+registerSW({
+  immediate: true,  // This will trigger the SW registration immediately
+});
